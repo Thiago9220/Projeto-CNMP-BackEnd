ff260183a0bd8203c427a67fcae7454e047357b9
Thiago Ramos <63021624+Thiago9220@users.noreply.github.com>
2024-08-13 21:33:00 -0300

    Adicionando modificações

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Permitir acesso apenas do frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/indicadores/")
+def get_indicadores():
+    indicadores = [
+        {"id": 89, "codigo": "BIBLIO I.1", "nome": "Quantidade de Empréstimos", "area": "BIBLIO Biblioteca", "unidade": "Qtd.", "classificador": "Monitoramento Operacional", "grupo": "-", "responsavel": "Igor Guevara"},
+        {"id": 90, "codigo": "BIBLIO I.2", "nome": "Quantidade de Demanda Reprimida", "area": "BIBLIO Biblioteca", "unidade": "Qtd.", "classificador": "Monitoramento Operacional", "grupo": "-", "responsavel": "Sávio Neves do Nascimento"},
+        # Adicione mais indicadores aqui
+    ]
+    return indicadores
